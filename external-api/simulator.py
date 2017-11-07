b3cd771354babd735673724918b4b510d8773b54
bgeils <bgeils@iastate.edu>
2017-11-07 00:13:09 -0600

    simulator:

---
+++ external-api/simulator.py
@@ -0,0 +1,51 @@
+'''
+Simulates a home that is consuming energy
+'''
+import requests, string
+import datetime, time
+import random
+
+random_device_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(16))
+
+def consume_energy(energy_range, device_id=random_device_id):
+    '''
+    Consume energy each time the unit is called.
+    :param energy_range: tuple, range to consume energy in watts
+    :return:
+    '''
+    rand_energy = random.randrange(energy_range[0], energy_range[1])
+
+    data = {
+        "device_id" : device_id,
+        "start_time" : str(datetime.datetime.utcnow()),
+        "duration" : 15,
+        "energy" : rand_energy
+    }
+    print(data)
+    r = requests.post('http://localhost:5000/insert_consum_data', json=data)
+    return r.status_code
+
+def produce_energy(energy_range):
+    '''
+    Simulate a device producing energy
+    :param energy_range:
+    :return:
+    '''
+    # TODO
+
+if __name__ == "__main__":
+    print('starting simulator.')
+    '''
+    Use the following device_id for testing:
+    4NH5SOZMPNIDR7FK
+    consume_energy(consume_range, device_id=4NH5SOZMPNIDR7FK)
+    '''
+
+    consume_range = (15,30) # watts to consume
+
+    try:
+        while True:
+            consume_energy(consume_range)
+            time.sleep(3)
+    except KeyboardInterrupt:
+        print('simulator exited!')
--- external-api/src/energy_post.py
+++ external-api/src/energy_post.py
@@ -1,4 +1,5 @@
 import pymongo
+import datetime
 
 client = pymongo.MongoClient("mongodb://bubbles:ybQWqmc97zHnrBYxKZLeghrNM44h@\
 ec2-35-167-132-139.us-west-2.compute.amazonaws.com:27017/production")
@@ -30,6 +31,10 @@ def insert_consum_data(device_id, start_time, duration, energy):
     :return: true or false
     '''
 
+    # 2017-11-06 14:32:57.935540
+    date_format = "%Y-%m-%d %H:%M:%S.%f"
+    start_time = datetime.datetime.strptime(start_time, date_format)
+
     return db.consum.insert_one({
         "device_id": device_id,
         "start_time": start_time,
@@ -37,3 +42,5 @@ def insert_consum_data(device_id, start_time, duration, energy):
         "energy": energy
          })
 
+if __name__ == "__main__":
+    pass
